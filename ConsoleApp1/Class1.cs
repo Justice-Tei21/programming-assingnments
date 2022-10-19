1c694dbe7161551100541f2e11cb4b6458a65712
Justice-Tei21 <jusnwa050826@student.jenseneducation.se>
2022-10-19 11:46:50 +0200

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+
+namespace spacey
+{
+	public class Class1
+	{
+
+		private int crazies = 0;
+		private Random random = new Random();
+		private string[] repository;
+
+
+
+        public int Crazies
+		{
+			get { return crazies; }
+			set { crazies = (value + 1)!; }
+		}
+		public Class1(int people)
+		{
+			crazies = people;
+			repository = new string[people];
+			individuals();
+		}
+
+		private void individuals()
+		{
+			string[] firstName = { "Philip", "Michael", "Franklin", "Trevor", "Judas","Jam","Mario" };
+			string[] lastName = { "Costa", "Iscariot", "Smith", "Plumber", "Webb","M. Mario" };
+
+			for(int i = 0; i < crazies; ++i)
+			{
+				
+				repository[i] = firstName[random.Next() % firstName.Length] +" " 
+					+ lastName[random.Next() % lastName.Length];
+
+			}
+
+		}
+
+		public void getpeople()
+		{
+			for(int i = 0; i < repository.Length; i++)
+			{
+				Console.WriteLine(repository[i]);
+			}
+		}
+	}
+}
---
+++ ConsoleApp1/inputchecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Inputchecker
+{
+
+	string input;
+
+
+	bool isvalidpassword;
+	public Inputchecker()
+	{
+		Console.WriteLine("Enter password");
+		ReadInput();
+
+
+
+	}
+
+	private void ReadInput()
+	{
+		input=Console.ReadLine();
+	}
+
+	private void Checkpassword()
+	{
+		string pattern = @"(){4,20}";
+
+	}
+
+}
